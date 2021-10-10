09ca27466edacdf0f9eb9663ad6c903967a0bdbe
Junbeom Park <junbum0674@gmail.com>
2021-10-10 20:05:02 +0900

    21.10.10 junbeom mapping add

--- src/main/java/main/IndexController.java
+++ src/main/java/main/IndexController.java
@@ -8,7 +8,29 @@ public class IndexController {
 	@RequestMapping("/index.do")
 	public String index() {
 		return "index"; //tiles설정 파일에 등록된 view의 이름
+	
 	}
+	@RequestMapping("/contact.do")
+	public String contact() {
+		return "contact";
+	}
+	@RequestMapping("/login.do")
+	public String login() {
+		return "login";
+	}
+	@RequestMapping("/sign.do")
+	public String sign() {
+		return "sign";
+	}
+	@RequestMapping("/place.do")
+	public String place() {
+		return "place";
+	}
+	@RequestMapping("/metropolygon.json")
+	public String metropolygon() {
+		return "metropolygon";
+	}
+	
 	
 //	@RequestMapping("/emp/login.do")
 //	public String advancedView() {
